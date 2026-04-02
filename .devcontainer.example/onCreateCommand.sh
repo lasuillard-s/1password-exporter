9334bebed1f1ad36b84d54d04b2cb1a09b474221
Yuchan Lee <lasuillard@gmail.com>
2026-04-02 20:44:13 +0900

    Dev-env as example (#79)

--- .devcontainer.example/onCreateCommand.sh
+++ .devcontainer.example/onCreateCommand.sh
@@ -2,17 +2,16 @@
 
 ARCH="$(dpkg --print-architecture)"
 
-CARGO_BINSTALL_VERSION="1.8.0"
-
-# 1Password
+# 1Password CLI
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg \
   && echo "deb [arch=${ARCH} signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/${ARCH} stable main" | sudo tee /etc/apt/sources.list.d/1password.list
 
 sudo apt-get update && sudo apt-get install -y \
   1password-cli
 
-curl -fsSL "https://github.com/cargo-bins/cargo-binstall/releases/download/v${CARGO_BINSTALL_VERSION}/cargo-binstall-$(rustc -vV | sed -n 's|host: ||p').tgz" \
-    | tar --extract --gzip --directory "${CARGO_HOME}/bin"
+# cargo-binstall
+curl --proto '=https' --tlsv1.2 --silent --fail --show-error --location https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
+    | bash
 
 # Download dev tools binaries
 cargo binstall -y --log-level debug \
